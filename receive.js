6198066ba427bd021f2cd0afd6378dd0b77157b7
akagaeng <akagaeng@gmail.com>
2020-04-03 19:41:00 +0900

    Format with prettier / eslint

--- receive.js
+++ receive.js
@@ -1,81 +1,86 @@
-const amqp = require('amqplib');
+const amqp = require("amqplib");
 
-const QUEUE_NAME = 'queue';
-const WORKING_SECONDS_MAX = 5;  // seconds
+const QUEUE_NAME = "queue";
+const WORKING_SECONDS_MAX = 5; // seconds
 
-const hostname = 'rabbit:rabbit@localhost';
+const hostname = "rabbit:rabbit@localhost";
 
 function sleep(secs) {
-    return new Promise(resolve => setTimeout(resolve, secs * 1000));
+  return new Promise(resolve => setTimeout(resolve, secs * 1000));
 }
 
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // time-intensive work
 async function timeIntensiveWork(secMax) {
-    const secs = getRndInteger(1, secMax);
-    process.stdout.write(`Working [${secs}s]`);
-    for (let i = 1; i <= secs; i++) {
-        process.stdout.write('.');
-        await sleep(1);
-    }
-    process.stdout.write('[DONE] \n');
+  const secs = getRndInteger(1, secMax);
+  process.stdout.write(`Working [${secs}s]...`);
+  await sleep(secs);
+  process.stdout.write("[DONE] \n");
 }
 
 async function startReceiveMessage(queue, channel) {
-    return new Promise(resolve => {
-        return channel.consume(queue, async (msg) => {
-            resolve(msg);
-        }, {noAck: false})
-    })
+  return new Promise(resolve => {
+    return channel.consume(
+      queue,
+      async msg => {
+        resolve(msg);
+      },
+      { noAck: false }
+    );
+  });
 }
 
 async function stopReceiveMessage(consumerTag, channel) {
-    return await channel.cancel(consumerTag)
-        .catch(console.error);
+  return await channel.cancel(consumerTag).catch(console.error);
 }
 
 async function waitForMessage(queue, channel) {
-    // Recovering unacked messages
-    await channel.recover();
+  // Recovering unacked messages
+  await channel.recover();
 
-    return await startReceiveMessage(queue, channel)
-        .then(async msg => {
-            const message = msg.content.toString();
-            const consumerTag = msg.fields.consumerTag;
-            process.stdout.write(`>> Received message: ${message}  consumerTag: ${consumerTag} \t`);
+  return await startReceiveMessage(queue, channel).then(async msg => {
+    const message = msg.content.toString();
+    const { consumerTag } = msg.fields;
+    process.stdout.write(
+      `>> Received message: ${message}  consumerTag: ${consumerTag} \t`
+    );
 
-            // send ACK
-            channel.ack(msg);
+    // send ACK
+    channel.ack(msg);
 
-            if (message && consumerTag) {
-                // Let's go for work!
+    if (message && consumerTag) {
+      // Let's go for work!
 
-                // Stop consuming
-                await stopReceiveMessage(consumerTag, channel);
+      // Stop consuming
+      await stopReceiveMessage(consumerTag, channel);
 
-                // Time intensive work (seconds)
-                await timeIntensiveWork(WORKING_SECONDS_MAX);
+      // Time intensive work (seconds)
+      await timeIntensiveWork(WORKING_SECONDS_MAX);
 
-                // Go back to wait for message
-                return waitForMessage(queue, channel);
-            }
-        })
+      // Go back to wait for message
+      return waitForMessage(queue, channel);
+    }
+  });
 }
 
 const open = amqp.connect(`amqp://${hostname}`);
 
 open
-    .then((connection) => {
-        return connection.createChannel()
-            .then(async channel => {
-                await channel.assertQueue(QUEUE_NAME, {durable: true});
-                console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", QUEUE_NAME);
-                return channel;
-            })
-            .then(async channel => await waitForMessage(QUEUE_NAME, channel))
-            .catch(console.error)
-    })
-    .catch(console.error);
+  .then(connection =>
+    connection
+      .createChannel()
+      .then(async channel => {
+        await channel.assertQueue(QUEUE_NAME, { durable: true });
+        console.log(
+          " [*] Waiting for messages in %s. To exit press CTRL+C",
+          QUEUE_NAME
+        );
+        return channel;
+      })
+      .then(async channel => await waitForMessage(QUEUE_NAME, channel))
+      .catch(console.error)
+  )
+  .catch(console.error);
--- send.js
+++ send.js
@@ -1,37 +1,36 @@
-const amqp = require('amqplib');
+const amqp = require("amqplib");
 
-const QUEUE_NAME = 'queue';
-
+const QUEUE_NAME = "queue";
 const count = Number(process.argv[2]);
-const hostname = 'rabbit:rabbit@localhost';
+const hostname = "rabbit:rabbit@localhost";
 
 function sendMessage(channel, queue, msg) {
-    channel.sendToQueue(queue, Buffer.from(msg));
-    setTimeout(function () {
-        channel.close();
-        process.exit(0);
-    }, 500);
+  channel.sendToQueue(queue, Buffer.from(msg));
+  setTimeout(() => {
+    channel.close();
+    process.exit(0);
+  }, 500);
 }
 
-if (isNaN(count) || count < 1) {
-    console.log('[Error] Input message count in argv[2].');
-    process.exit(0);
+if (Number.isNaN(count) || count < 1) {
+  console.log("[Error] Input message count in argv[2].");
+  process.exit(0);
 }
 
 const open = amqp.connect(`amqp://${hostname}`);
 
 // Publisher
-open.then(connection => connection.createChannel())
-    .then(channel => channel.assertQueue(QUEUE_NAME)
-        .then(ok => {
-                if (ok) {
-                    for (let i = 1; i <= count; i++) {
-                        sendMessage(channel, QUEUE_NAME, `MSG[${i}]`);
-                    }
-                }
+open
+  .then(connection => connection.createChannel())
+  .then(channel =>
+    channel.assertQueue(QUEUE_NAME).then(ok => {
+      if (ok) {
+        for (let i = 1; i <= count; i++) {
+          sendMessage(channel, QUEUE_NAME, `MSG[${i}]`);
+        }
+      }
 
-                console.log(`${count} messages sent`);
-            }
-        ))
-    .catch(console.warn);
-
+      console.log(`${count} messages sent`);
+    })
+  )
+  .catch(console.warn);
